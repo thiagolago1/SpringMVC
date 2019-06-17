d202c37381df4ee0c360cde8606a24ba2aa1dca3
Thiago Lago <thiagolagoo2@gmail.com>
2019-06-17 18:47:12 -0300

    :tada: [Add] frist commit with some things

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -0,0 +1,25 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//Configurações básicas
+public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {AppWebConfiguration.class, JPAConfiguration.class};
+	}
+
+	@Override
+	//Mapeando o servlet com o spring
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+	
+}
---
+++ casadocodigo/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Livros - Casa do Código</title>
+</head>
+<body>
+<h1>Casa do Código</h1>
+	<table>
+		<tr>
+			<td>Java 8 Pŕatico</td>
+			<td>Certificação OCJP</td>
+		</tr>
+		<tr>
+			<td>TDD Java</td>
+			<td>Android</td>
+		</tr>
+	</table>
+</body>
+</html>
