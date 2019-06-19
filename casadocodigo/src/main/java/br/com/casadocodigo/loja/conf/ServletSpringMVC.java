68d4bf8e4a9f480975826e5e5e25e24e17cbb47d
Thiago Lago <thiagolagoo2@gmail.com>
2019-06-18 21:19:01 -0300

    :bug: [Fix] Encoding e Controller

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.conf;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 //Configurações básicas
@@ -19,6 +22,13 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 	//Mapeando o servlet com o spring
 	protected String[] getServletMappings() {
 		return new String[] {"/"};
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+		encodingFilter.setEncoding("UTF-8"); //Toda requisição que chegar no Spring, ele transforma pra utf-8
+		return new Filter[] {encodingFilter};
 	}
 
 	
